ccb0557bc1563a19c7d42482e483553d8a6512a8
Łukasz <ukasz@DESKTOP-6RF5HR6>
2017-06-03 10:07:21 +0200

    stable version with gui

--- test/bke-project-client/src/main/java/client/Client.java
+++ test/bke-project-client/src/main/java/client/Client.java
@@ -1,52 +1,195 @@
 package client;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.Socket;
+
+import java.net.*;
+import java.security.KeyManagementException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.io.*;
+import java.util.*;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
-import server.Protocol;
+
+import entity.ChatMessage;
+import gui.ClientGUI;
 
 public class Client {
 
-	public static void main(String[] args) throws Exception {
+	private ObjectInputStream sInput;
+	private ObjectOutputStream sOutput;
+	private Socket socket;
+
+	private ClientGUI clientGui;
+
+	private String server, username;
+	private int port;
+
+	public Client(String server, int port, String username) {
+		this(server, port, username, null);
+	}
+
+	public Client(String server, int port, String username, ClientGUI cg) {
+		this.server = server;
+		this.port = port;
+		this.username = username;
+		this.clientGui = cg;
+	}
+
+	public boolean start() {
+		try {
+			initializeClientSocket();
+		} catch (Exception ec) {
+			display("Error connectiong to server:" + ec);
+			return false;
+		}
+
+		String msg = "Connection accepted " + socket.getInetAddress() + ":" + socket.getPort();
+		display(msg);
+
+		try {
+			sInput = new ObjectInputStream(socket.getInputStream());
+			sOutput = new ObjectOutputStream(socket.getOutputStream());
+		} catch (IOException eIO) {
+			display("Exception creating new Input/output Streams: " + eIO);
+			return false;
+		}
+
+		new ListenFromServer().start();
+		try {
+			sOutput.writeObject(username);
+		} catch (IOException eIO) {
+			display("Exception doing login : " + eIO);
+			disconnect();
+			return false;
+		}
+		return true;
+	}
+
+	private void initializeClientSocket() throws KeyStoreException, IOException, NoSuchAlgorithmException,
+			CertificateException, FileNotFoundException, KeyManagementException, UnknownHostException {
 		char[] password = { 't', 'e', 's', 't', '1', '2', '3'};
 		KeyStore ks = KeyStore.getInstance("PKCS12");
 		try (InputStream input = new FileInputStream("client.pfx")) {
 			ks.load(input, password);
 		}
-
 		TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
 		tmf.init(ks);
 		TrustManager[] trustManagers = tmf.getTrustManagers();
-
 		SSLContext sslContext = SSLContext.getInstance("TLS");
 		sslContext.init(null, trustManagers, null);
-
 		SSLSocketFactory factory = sslContext.getSocketFactory();
-		Protocol protocol = new Protocol();
-		try (Socket socket = factory.createSocket("localhost", 12345)) {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-			
-			//String loginCommand = protocol.buildLoginCommand("test", "test");
-			writer.write("#LOGIN#test;test");
-/*			String response = reader.readLine();
-			if(response.equals(Boolean.TRUE.toString())) {
-				writer.write(protocol.buildMessageCommand("siema"));
-			} else {
-				throw new RuntimeException("Client disconected ! Invalid authorization");
-			}*/
-			writer.flush();
+		socket = factory.createSocket(server, port);
+	}
 
+	private void display(String msg) {
+		if (clientGui == null)
+			System.out.println(msg);
+		else
+			clientGui.append(msg + "\n");
+	}
+
+	public void sendMessage(ChatMessage msg) {
+		try {
+			sOutput.writeObject(msg);
+		} catch (IOException e) {
+			display("Exception writing to server: " + e);
 		}
 	}
 
+	private void disconnect() {
+		try {
+			if (sInput != null)
+				sInput.close();
+		} catch (Exception e) {
+		}
+		try {
+			if (sOutput != null)
+				sOutput.close();
+		} catch (Exception e) {
+		}
+		try {
+			if (socket != null)
+				socket.close();
+		} catch (Exception e) {
+		}
+		if (clientGui != null)
+			clientGui.connectionFailed();
+
+	}
+
+	public static void main(String[] args) {
+		int portNumber = 1500;
+		String serverAddress = "localhost";
+		String userName = "Anonymous";
+
+		switch (args.length) {
+		case 3:
+			serverAddress = args[2];
+		case 2:
+			try {
+				portNumber = Integer.parseInt(args[1]);
+			} catch (Exception e) {
+				System.out.println("Invalid port number.");
+				System.out.println("Usage is: > java Client [username] [portNumber] [serverAddress]");
+				return;
+			}
+		case 1:
+			userName = args[0];
+		case 0:
+			break;
+		default:
+			System.out.println("Usage is: > java Client [username] [portNumber] {serverAddress]");
+			return;
+		}
+		Client client = new Client(serverAddress, portNumber, userName);
+		if (!client.start())
+			return;
+
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			System.out.print("> ");
+			String msg = scan.nextLine();
+			if (msg.equalsIgnoreCase("LOGOUT")) {
+				client.sendMessage(new ChatMessage(ChatMessage.LOGOUT, ""));
+				break;
+			}
+			else if (msg.equalsIgnoreCase("WHOISIN")) {
+				client.sendMessage(new ChatMessage(ChatMessage.WHOISIN, ""));
+			} else {
+				client.sendMessage(new ChatMessage(ChatMessage.MESSAGE, msg));
+			}
+		}
+		scan.close();
+		client.disconnect();
+	}
+
+	class ListenFromServer extends Thread {
+
+		public void run() {
+			while (true) {
+				try {
+					String msg;
+					if((msg = (String)sInput.readObject()) != null) {
+						if (clientGui == null) {
+							System.out.println(msg);
+							System.out.print("> ");
+						} else {
+							clientGui.append(msg);
+						}
+					}
+				} catch (IOException e) {
+					display("Server has close the connection: " + e);
+					if (clientGui != null)
+						clientGui.connectionFailed();
+					break;
+				}
+				catch (ClassNotFoundException e2) {
+				}
+			}
+		}
+	}
 }
---
+++ test/bke-project-client/src/main/java/gui/ClientGUI.java
@@ -0,0 +1,155 @@
+package gui;
+
+import javax.swing.*;
+
+import client.Client;
+import entity.ChatMessage;
+
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class ClientGUI extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	private JLabel label;
+	private JTextField messageField;
+	private JTextField serverField, portField;
+	private JButton login, logout, whoIsIn;
+	private JTextArea textArea;
+	private boolean connected;
+	private Client client;
+	private int defaultPort;
+	private String defaultHost;
+
+	ClientGUI(String host, int port) {
+
+		super("Chat Client");
+		defaultPort = port;
+		defaultHost = host;
+		
+		JPanel northPanel = new JPanel(new GridLayout(3,1));
+		JPanel serverAndPort = new JPanel(new GridLayout(1,5, 1, 3));
+		serverField = new JTextField(host);
+		portField = new JTextField("" + port);
+		portField.setHorizontalAlignment(SwingConstants.RIGHT);
+
+		serverAndPort.add(new JLabel("Server Address:  "));
+		serverAndPort.add(serverField);
+		serverAndPort.add(new JLabel("Port Number:  "));
+		serverAndPort.add(portField);
+		serverAndPort.add(new JLabel(""));
+		northPanel.add(serverAndPort);
+
+		label = new JLabel("Enter your username below", SwingConstants.CENTER);
+		northPanel.add(label);
+		messageField = new JTextField("Anonymous");
+		messageField.setBackground(Color.WHITE);
+		northPanel.add(messageField);
+		add(northPanel, BorderLayout.NORTH);
+
+		textArea = new JTextArea("Welcome to the Chat room\n", 80, 80);
+		JPanel centerPanel = new JPanel(new GridLayout(1,1));
+		centerPanel.add(new JScrollPane(textArea));
+		textArea.setEditable(false);
+		add(centerPanel, BorderLayout.CENTER);
+
+		login = new JButton("Login");
+		login.addActionListener(this);
+		logout = new JButton("Logout");
+		logout.addActionListener(this);
+		logout.setEnabled(false);
+		whoIsIn = new JButton("Who is in");
+		whoIsIn.addActionListener(this);
+		whoIsIn.setEnabled(false);
+
+		JPanel southPanel = new JPanel();
+		southPanel.add(login);
+		southPanel.add(logout);
+		southPanel.add(whoIsIn);
+		add(southPanel, BorderLayout.SOUTH);
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(600, 600);
+		setVisible(true);
+		messageField.requestFocus();
+
+	}
+
+	// method for client to append the text
+	public void append(String str) {
+		textArea.append(str);
+		textArea.setCaretPosition(textArea.getText().length() - 1);
+	}
+
+	public void connectionFailed() {
+		login.setEnabled(true);
+		logout.setEnabled(false);
+		whoIsIn.setEnabled(false);
+		label.setText("Enter your username below");
+		messageField.setText("Anonymous");
+		portField.setText("" + defaultPort);
+		serverField.setText(defaultHost);
+		serverField.setEditable(false);
+		portField.setEditable(false);
+		messageField.removeActionListener(this);
+		connected = false;
+	}
+		
+	public void actionPerformed(ActionEvent e) {
+		Object o = e.getSource();
+		if(o == logout) {
+			client.sendMessage(new ChatMessage(ChatMessage.LOGOUT, ""));
+			return;
+		}
+		if(o == whoIsIn) {
+			client.sendMessage(new ChatMessage(ChatMessage.WHOISIN, ""));				
+			return;
+		}
+		if(connected) {
+			client.sendMessage(new ChatMessage(ChatMessage.MESSAGE, messageField.getText()));				
+			messageField.setText("");
+			return;
+		}
+
+		if(o == login) {
+			String username = messageField.getText().trim();
+			if(username.length() == 0)
+				return;
+			String server = serverField.getText().trim();
+			if(server.length() == 0)
+				return;
+			String portNumber = portField.getText().trim();
+			if(portNumber.length() == 0)
+				return;
+			int port = 0;
+			try {
+				port = Integer.parseInt(portNumber);
+			}
+			catch(Exception en) {
+				return;
+			}
+
+			client = new Client(server, port, username, this);
+			if(!client.start()) 
+				return;
+			messageField.setText("");
+			label.setText("Enter your message below");
+			connected = true;
+			
+			login.setEnabled(false);
+			logout.setEnabled(true);
+			whoIsIn.setEnabled(true);
+			serverField.setEditable(false);
+			portField.setEditable(false);
+			messageField.addActionListener(this);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		new ClientGUI("localhost", 1500);
+	}
+
+}
+
